c931940f7da5d0ed11fc5e1cc7a5bf3c50878b0a
Learkedins <learkedins@gmail.com>
2022-12-01 19:59:16 +0700

    add command handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 // Import 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
 
 // Make a new client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Collection for the commands
+client.commands = new Collection();
+
+// Import all commands
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] the command at ${filePath} is missing a "data" or "execute" property`)
+  }
+}
 
 // Run this code when the client is ready
 // Use c as the event parameter
@@ -11,6 +31,11 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+// Handling interaction events
+client.on(Events.InteractionCreate, interaction => {
+  if(!interaction.isChatInputCommand()) return;
+  console.log(interaction)
+}
+
 // Log in to Discord with your client's token
 client.login(token);
-
